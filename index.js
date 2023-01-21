01bb46adaeff225725c2f7e45d99631336adafb2
zhiktang <zhikai.tang123@gmail.com>
2023-01-21 13:52:13 -0600

    gimme badge

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { token, pathToModel } = require('./config.json');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const {exec} = require('child_process');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+botBusy = false;
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 client.login(token);
- 
