733186c354c1767a84e0ea846f7b35a37387d4a3
zhiktang <zhikai.tang123@gmail.com>
2023-01-21 14:20:10 -0600

    I don't even care anymore

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const { token} = require('./config.json');
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
